e2f68379a7b6f8550dabe490c9880b89c92918c2
darkssr <cweiting1987@gmail.com>
2024-02-19 16:22:56 +0800

    test
    test

--- run-new-bz2.sh
+++ run-new-bz2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-scanpackages -m ./debs > Packages
-rm -r -f /Users/tashigefengzi/Desktop/github/yilai/Packages.bz2
+rm -r -f ./Packages.bz2
 bzip2 Packages
 dpkg-scanpackages -m ./debs > Packages
